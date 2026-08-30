897c674134d4fb12851ef46e800e182a579bfa32
Sebastian Padilla <sebaz4825@gmail.com>
2026-08-30 17:54:07 -0500

    add To Check

--- Rhombus/Rhombus/Program.cs
+++ Rhombus/Rhombus/Program.cs
@@ -9,29 +9,31 @@ do
     do
     {
         n = ConsoleExtension.GetInt("Ingrese el tamaño del rombo:  ");
-        if (n < 3 || n % 2 == 0) ;
+        if (n < 3 || n % 2 == 0)
         {
             Console.WriteLine("El tamaño debe ser un número impar mayor o igual a 3.");
         }
-    } while (n < 3 || n % 2 == 0);
+    }
+    while (n < 3 || n % 2 == 0);
+
     int m = n / 2;
 
     for (int i = 0; i < n; i++)
     {
         int d = Math.Abs(m - i);
-        int outerSpace = d;
+        int outerSpaces = d;
+
         if (d == m)
         {
-            Console.WriteLine($"{new string(' ', outerSpace)}#");
+            Console.WriteLine($"{new string(' ', outerSpaces)}#");
         }
         else
         {
             int innerSpaces = 2 * (m - d) - 1;
-            Console.WriteLine($"{new string(' ', outerSpace)}#{new string(' ', innerSpaces)}");
+            Console.WriteLine($"{new string(' ', outerSpaces)}#{new string(' ', innerSpaces)}#");
         }
     }
 
-//final
     do
     {
     answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]i, [N]o?: ", options);
