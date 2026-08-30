d53d3356e41876c0f2bab5faf04e6c6bdbcd5452
Sebastian Padilla <sebaz4825@gmail.com>
2026-08-30 17:34:29 -0500

    add test 1

---
+++ Rhombus/Rhombus/Program.cs
@@ -0,0 +1,42 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    int n;
+    do
+    {
+        n = ConsoleExtension.GetInt("Ingrese el tamaño del rombo:  ");
+        if (n < 3 || n % 2 == 0) ;
+        {
+            Console.WriteLine("El tamaño debe ser un número impar mayor o igual a 3.");
+        }
+    } while (n < 3 || n % 2 == 0);
+    int m = n / 2;
+
+    for (int i = 0; i < n; i++)
+    {
+        int d = Math.Abs(m - i);
+        int outerSpace = d;
+        if (d == m)
+        {
+            Console.WriteLine($"{new string(' ', outerSpace)}#");
+        }
+        else
+        {
+            int innerSpaces = 2 * (m - d) - 1;
+            Console.WriteLine($"{new string(' ', outerSpace)}#{new string(' ', innerSpaces)}");
+        }
+    }
+
+//final
+    do
+    {
+    answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]i, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+}
+while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase)) ;
+Console.WriteLine("Gamer Over.");
